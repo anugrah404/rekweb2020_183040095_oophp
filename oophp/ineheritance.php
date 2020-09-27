1f8f24f0ff75fb6be434a70e1d3474c271221e0f
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2020-09-27 13:25:21 +0700

    Update ineheritance.php

--- oophp/ineheritance.php
+++ oophp/ineheritance.php
@@ -64,6 +64,7 @@ class CetakInfoProduk
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
 $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 
+
 echo $produk1->getInfoLengkap();
 echo "<br>";
 echo $produk2->getInfoLengkap();
